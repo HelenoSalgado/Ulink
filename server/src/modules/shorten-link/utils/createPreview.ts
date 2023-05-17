9578c8a97e9979ee16acabdd0d0e54f4d633ff24
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2023-05-17 12:15:47 -0300

    excluindo meta tag html

--- server/src/modules/shorten-link/utils/createPreview.ts
+++ server/src/modules/shorten-link/utils/createPreview.ts
@@ -21,7 +21,6 @@ export default async function createPreview(head: CreateLinkDto){
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
       <script>
       (async () => {
         await fetch('https://api-analytics.onrender.com/analytics', {
